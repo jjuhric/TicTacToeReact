eab4ed54bfcbad4c0f275dec103c1985e2d6c791
Jeff Uhrick <jjuhric@gmail.com>
2024-04-08 00:13:25 -0500

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import { GameBoard } from "./components/GameBoard";
 import { Player } from "./components/Player";
-import { WINNING_COMBINATIONS } from "./winning-combinations";
 import Log from "./components/Log";
 import GameOver from "./components/GameOver";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+};
+
+const INITIAL_GAMEBOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
 
 function getActivePlayer(gameTurns) {
   let currentPlayer = 'X';
@@ -13,38 +24,44 @@ function getActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-
-function App() {
-  const [players, setPlayers] = useState({'X': 'Player 1', 'O': 'Player 2'}); // {'X': 'Player 1', 'O': 'Player 2
-  const [gameTurns, setGameTurns] = useState([]);
-
-  const activePlayer = getActivePlayer(gameTurns);
-
-  const initialGameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-  let gameBoard = [...initialGameboard.map(row => [...row])]; //Deep copy of the initial game board
-
+function deriveWinner(gameBoard) {
   let winner;
-
-  for (const turn of gameTurns) {
-    const { square: { row, col }, player } = turn;
-    gameBoard[row][col] = player;
-  }
 
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquare = gameBoard[combination[0].row][combination[0].col];
     const secondSquare = gameBoard[combination[1].row][combination[1].col];
     const thirdSquare = gameBoard[combination[2].row][combination[2].col];
 
-    if(firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare) {
+    if (firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare) {
       winner = firstSquare;
       break;
     }
   }
+
+  return winner;
+};
+
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAMEBOARD.map(row => [...row])]; //Deep copy of the initial game board
+
+  for (const turn of gameTurns) {
+    const { square: { row, col }, player } = turn;
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS); // {'X': 'Player 1', 'O': 'Player 2
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = getActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard);
 
   const isDraw = gameTurns.length === 9 && !winner;
 
@@ -74,10 +91,10 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.O} symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
         </ol>
-        {(winner || isDraw) && <GameOver winner={players[winner]} onRestart={handleReset}/>}
+        {(winner || isDraw) && <GameOver winner={players[winner]} onRestart={handleReset} />}
         <GameBoard
           setActivePlayer={handleChangingTurns}
           board={gameBoard}
